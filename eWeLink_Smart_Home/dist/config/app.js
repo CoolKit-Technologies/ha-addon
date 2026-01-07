a801ec2e37a9c12c65f7ce09b7e1c2802a91ad32
SuiKa <44567811+SuiKaSan@users.noreply.github.com>
2026-01-07 21:40:14 +0800

    feat: uograde to v1.4.6

--- eWeLink_Smart_Home/dist/config/app.js
+++ eWeLink_Smart_Home/dist/config/app.js
@@ -6,13 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.appSecret = exports.appId = void 0;
 var config_1 = require("./config");
 var process_1 = __importDefault(require("process"));
-var appId = '4s1FXKC9FaGfoqXhmXSJneb3qcm1gOak';
+var appId = 'iY6iazKsHokdS9FSci9AKbWTMCXUchaf';
 exports.appId = appId;
-var appSecret = 'oKvCM06gvwkRbfetd6qWRrbC3rFrbIpV';
+var appSecret = 'JBLkhfhNH6kiLD1xnsiDCaXPZn4qhi1O';
 exports.appSecret = appSecret;
 exports.appId = appId = process_1.default.env.CK_API_ENV === 'test' ? 'iY6iazKsHokdS9FSci9AKbWTMCXUchaf' : '4s1FXKC9FaGfoqXhmXSJneb3qcm1gOak';
 exports.appSecret = appSecret = process_1.default.env.CK_API_ENV === 'test' ? 'JBLkhfhNH6kiLD1xnsiDCaXPZn4qhi1O' : 'oKvCM06gvwkRbfetd6qWRrbC3rFrbIpV';
 if (config_1.debugMode) {
-    exports.appId = appId = 'iY6iazKsHokdS9FSci9AKbWTMCXUchaf';
-    exports.appSecret = appSecret = 'JBLkhfhNH6kiLD1xnsiDCaXPZn4qhi1O';
+    exports.appId = appId = '4s1FXKC9FaGfoqXhmXSJneb3qcm1gOak';
+    exports.appSecret = appSecret = 'oKvCM06gvwkRbfetd6qWRrbC3rFrbIpV';
 }
